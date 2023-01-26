7ae0be3fb4fe87e3b00d4735e0a853030c00661d
S4sunny <srocks023@gmail.com>
2023-01-26 22:06:51 +0530

    'test'

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from sensor.pipeline.training_pipeline import start_training_pipeline
-from sensor.pipeline.batch_prediction import start_batch_prediction
+from sensor.logger import logging
+from sensor.exception import SensorException
+from sensor import utils
+import sys,os
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
-file_path="/config/workspace/aps_failure_training_set1.csv"
-print(__name__)
+
 if __name__=="__main__":
      try:
-          #start_training_pipeline()
-          output_file = start_batch_prediction(input_file_path=file_path)
-          print(output_file)
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+
+        #DataValidation
+        
+        data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+        data_validation = DataValidation(data_validation_config=data_validation_config,
+                         data_ingestion_artifact=data_ingestion_artifact)
+        
+        data_validation_artifact = data_validation.initiate_data_validation()
+                          
      except Exception as e:
           print(e)
--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -1,6 +1,16 @@
 
-class DataIngestionArtifact:...
-class DataValidationArtifact:...
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    feature_store_file_path:str
+    train_file_path:str
+    test_file_path:str
+
+@dataclass
+class DataValidationArtifact:
+    report_file_path:str  
+
 class DataTransformationArtiArtifact:...
 class ModelTrainingArtifact:...
 class ModelTraiingArtifact:...
